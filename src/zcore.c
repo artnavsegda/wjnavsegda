4ca66e6660f27b4bbe6298220076866137b2dd29
Art Navsegda <artnavsegda@gmail.com>
2019-08-23 14:39:01 +0300

    more

--- src/zcore.c
+++ src/zcore.c
@@ -38,6 +38,26 @@ int setparameter(char * setiface, char * setparam, char * setvalue)
   WJEString(entity, parameter->name, WJE_SET, setvalue);
 }
 
+int arrlength(char **array)
+{
+  int length = 0;
+  while (array[length] != NULL)
+    length++;
+  return length;
+}
+
+int parse(char * stringtoparse)
+{
+  char *tokarr[100]; // maximum argument count
+  int i = 0;
+  tokarr[i] = stringtoparse;
+
+  while ((tokarr[i] = strtok(tokarr[i], " ")) != NULL)
+    tokarr[++i] = NULL;
+
+  printf("number of tokens %d\n", arrlength(tokarr));
+}
+
 int execute(char * line)
 {
   if (strcmp(line,"..")==0){
