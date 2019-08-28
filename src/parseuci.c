22ef11ce0e647ff2e2f7495111fb758904e95bf0
Art Navsegda <artnavsegda@gmail.com>
2019-08-28 12:13:42 +0300

    this

--- src/parseuci.c
+++ src/parseuci.c
@@ -23,10 +23,29 @@ int main(int argc, char **argv) {
   output = WJEArray(NULL, NULL, WJE_NEW);
   while (ifaceinput = _WJEObject(input,"values[]", WJE_GET, &ifaceinput))
   {
-    //puts(ifaceinput->name);
-    //WJEDump(ifaceinput);
     ifaceoutput = WJEObject(output, "interface", WJE_NEW);
+    puts(ifaceinput->name);
     WJEString(ifaceoutput,"name",WJE_NEW,ifaceinput->name);
+
+    char * macaddr = WJEString(ifaceinput,"macaddr",WJE_GET,"");
+    if (macaddr[0])
+    {
+      WJEString(ifaceoutput,"macaddr",WJE_NEW,macaddr);
+    }
+
+    int defaultroute = WJEInt32(ifaceinput,"defaultroute",WJE_GET,-1);
+    if (defaultroute != -1)
+    {
+      WJEBool(ifaceoutput,"defaultroute",WJE_NEW,(XplBool)defaultroute);
+    }
+
+    int metric = WJEInt32(ifaceinput,"metric",WJE_GET,-1);
+    if (metric != -1)
+    {
+      WJEInt32(ifaceoutput,"metric",WJE_NEW,metric);
+    }
+
+    WJEDump(ifaceinput);
   }
 
   WJEDump(output);
---
+++ src/shellread.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <wjelement.h>
+#include <wjreader.h>
+
+char * linefy(char * marker)
+{
+  char * pch = strpbrk(marker,"\n\t");
+  while (pch != NULL)
+  {
+    *pch = ' ';
+    pch = strpbrk(pch,"\n\t");
+  }
+}
+
+int main(int argc, char **argv) {
+  WJElement arg = NULL;
+  arg = WJEObject(NULL, NULL, WJE_NEW);
+  WJEString(arg, "config", WJE_SET, "network");
+  WJEString(arg, "type", WJE_SET, "interface");
+
+  char * something = WJEToString(arg, TRUE);
+  linefy(something);
+  puts(something);
+
+  
+
+}
